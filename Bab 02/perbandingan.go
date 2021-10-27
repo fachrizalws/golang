f4a9596482ebb7ff7df637d59dee95c6404b3ad0
fachrizalws <65730932+fachrizalws@users.noreply.github.com>
2021-10-27 15:15:16 +0700

    Create perbandingan.go

---
+++ Bab 02/perbandingan.go
@@ -0,0 +1,14 @@
+package main
+import “fmt”
+func main() {
+    var (
+        x = 6
+        y = 19
+    )
+    fmt.Println(x>y)
+    fmt.Println(x<y)
+	  fmt.Println(x>=y)
+    fmt.Println(x<=y)
+    fmt.Println(x!=y)
+    fmt.Println(x==y)
+}

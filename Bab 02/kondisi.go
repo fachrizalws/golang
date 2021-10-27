3d761364a5d9d0900e8743cc51d0bcdc296c1f38
fachrizalws <65730932+fachrizalws@users.noreply.github.com>
2021-10-28 05:49:08 +0700

    Create kondisi.go

---
+++ Bab 02/kondisi.go
@@ -0,0 +1,13 @@
+package main
+import “fmt”
+func main() {
+	var (
+	x = 6
+	y = 26
+	)
+	if x>y || x-y<x{
+	fmt.Println(“Nilai x lebih besar dari y atau saat x dikurangi y kurang dari x.”)
+	}else {
+	fmt.Println(“Nilai x kurang dari y.”)
+	}
+}

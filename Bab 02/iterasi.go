bfbc0b0021e8f6875832407a7a387e328e49fd41
fachrizalws <65730932+fachrizalws@users.noreply.github.com>
2021-10-28 05:54:33 +0700

    Create iterasi.go

---
+++ Bab 02/iterasi.go
@@ -0,0 +1,11 @@
+package main
+import “fmt”
+func main() {
+	// iterasi perintah while
+	// go tidak menyediakan sintaks “while”, yang digunakan adalah perintah “for”
+	i := 0
+	for i<6 {
+	fmt.Println(i)
+	i++
+	}
+}

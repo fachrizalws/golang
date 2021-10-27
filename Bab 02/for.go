f65eb906151a98fd5ecb34809144998f62c2fab1
fachrizalws <65730932+fachrizalws@users.noreply.github.com>
2021-10-28 05:54:07 +0700

    Create for.go

---
+++ Bab 02/for.go
@@ -0,0 +1,12 @@
+package main
+import “fmt”
+func main() {
+    // iterasi - perintah “for”
+    var i int
+    for i=0;i<6;i++ {
+        fmt.Println(i)
+    }
+    for j:=9;j< 12;j++ {
+        fmt.Println(j)
+    }
+}
